f52f28577c01cf8024513057d865c375df6a4d53
Danny Solomon N <ndanny366@gmail.com>
2024-04-11 21:19:34 +0530

    minor change in variable name

--- middleware/clientAuthentication.js
+++ middleware/clientAuthentication.js
@@ -1,14 +1,17 @@
 // Middleware to authenticate requests
 const authenticate = (req, res, next) => {
-  const { clientId, clientSecret } = req.headers;
+  const { clientid, clientsecret } = req.headers;
+
   // Check if client id and secret are present
-  if (!clientId || !clientSecret) {
+  if (!clientid || !clientsecret) {
     return res.status(401).json({
       error:
         "Authentication failed. Please provide client id and client secret.",
     });
   }
   // Check if client id and secret are valid
+  // not check now because we have no client info database
+
   next();
 };
 
